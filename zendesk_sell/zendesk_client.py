f2812d8e03a026a592a74b49c289c34d57f4aabb
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2021-01-12 17:33:20 +0100

    remove decorator in favour of shared session

--- zendesk_sell/zendesk_client.py
+++ zendesk_sell/zendesk_client.py
@@ -7,48 +7,50 @@ import requests
 
 logger = logging.getLogger(__name__)
 
+
 class ZendeskSell:
+    # static values
     _client_id: str
     _client_secret: str
+
+    # these will change as access_token expires
     _refresh_token: str
     _access_token: Optional[str] = None
     _access_token_ttl: Optional[float] = None
-    _session = None
-
-    class Decorators:
-        @staticmethod
-        def refresh_token(decorated):
-            # the function that is used to check
-            # the JWT and refresh if necessary
-            def wrapper(api, *args, **kwargs):
-                if api._access_token_ttl is None or time.time() > api._access_token_ttl:
-                    api._refresh_access_token()
-
-                return decorated(api, *args, **kwargs)
-
-            return wrapper
+    _session: Optional[requests.Session] = None
 
     def __init__(self, client_id: str, client_secret: str, refresh_token: str):
         self._client_id = client_id
         self._client_secret = client_secret
         self._refresh_token = refresh_token
-        self._session = requests.Session()
 
     def get_contacts(self, per_page: int = 100):
-        return self.call(
+        return self.do(
             "GET",
             "/v2/contacts",
             params=dict(sort_by="updated_at:desc", per_page=per_page),
         )
 
-    @Decorators.refresh_token
-    def call(self, method: str, path: str, *args, **kwargs) -> Dict:
-        headers = kwargs.get("headers", {})
-        headers["Authorization"] = f"Bearer {self._access_token}"
-        kwargs["headers"] = headers
+    def do(self, *args, **kwargs) -> Dict:
+        session = self.__session()
+        return self.__do(session, *args, **kwargs)
 
+    def __session(self) -> requests.Session:
+        if self._access_token_ttl is not None and time.time() < self._access_token_ttl:
+            assert self._session is not None
+            return self._session
+
+        token = self._refresh_access_token()
+
+        session = requests.Session()
+        session.headers.update({"Authorization": f"Bearer {token}"})
+        return session
+
+    def __do(
+        self, session: requests.Session, method: str, path: str, *args, **kwargs
+    ) -> Dict:
         try:
-            resp = self._session.request(
+            resp = session.request(
                 method, "https://api.getbase.com" + path, *args, **kwargs
             )
             resp.raise_for_status()
@@ -57,7 +59,7 @@ class ZendeskSell:
             logger.exception("failed to perform request")
             raise
 
-    def _refresh_access_token(self):
+    def _refresh_access_token(self) -> str:
         try:
             resp = requests.post(
                 "https://api.getbase.com/oauth2/token",
@@ -77,15 +79,17 @@ class ZendeskSell:
             self._access_token_ttl = float(data["expires_in"] - 100)
             self._refresh_token = data["refresh_token"]
 
-        # TODO: remove once we figure out how
-        with open("config.json", "w") as fp:
-            json.dump(
-                dict(
-                    client_id=self._client_id,
-                    client_secret=self._client_secret,
-                    refresh_token=self._refresh_token,
-                ),
-                fp,
-                indent="  ",
-                sort_keys=True,
-            )
+            # TODO: remove once we figure out how
+            with open("config.json", "w") as fp:
+                json.dump(
+                    dict(
+                        client_id=self._client_id,
+                        client_secret=self._client_secret,
+                        refresh_token=self._refresh_token,
+                    ),
+                    fp,
+                    indent="  ",
+                    sort_keys=True,
+                )
+            assert self._access_token is not None
+            return self._access_token
